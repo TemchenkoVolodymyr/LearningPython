32d078435104b3e8418caa90a8dd2511374574f7
VOVA TEMCHENKO <vovatemcenko4@gmail.com>
2024-08-19 12:43:19 +0200

    Lists and special symbols

--- Lists and operations/ex4.py
+++ Lists and operations/ex4.py
@@ -86,3 +86,108 @@
 
 
 
+## list() , if else , append() , len()
+# lst = list(input().split())
+# if lst[0] != lst[len(lst) - 1] :
+#     lst.append(True)
+#
+# else :
+#     lst.append(False)
+# print(*lst)
+
+## insert()
+
+# cities = ["Moscow", "London", "Kiev"]
+# cities.insert(1,"New York")
+# print(*cities)
+
+## list()  , remove() , while , in , join() , index()
+
+# tel = list(input())
+# tel.remove("+")
+# find_index = tel.index("7")
+# tel[find_index] = "8"
+# while "-" in tel :
+#     tel.remove("-")
+# print("".join(tel))
+
+##
+
+# fio = input()
+# first_name,middle_name,last_name = fio.split()
+# initials = f"{last_name} {first_name[0]}.{middle_name[0]}."
+# print(initials)
+
+## map() , while , remove() , append() , min()
+# numbers = list(map(int,input().split()))
+# a = 1
+# found_num = []
+# while a <= 3 :
+#     min_number = min(numbers)
+#     found_num.append(min_number)
+#     numbers.remove(min_number)
+#     a += 1
+#
+# print(*found_num)
+
+
+## if else , append(),pop()
+
+# lst = list(map(int, input().split()))
+# del_item = lst.pop()
+# if del_item % 2 == 0:
+#     lst.append(False)
+# else:
+#     lst.append(True)
+#
+# print(*lst)
+
+## count()
+
+# marks = list(map(int,input().split()))
+# a = marks.count(2)
+# print(a)
+
+## sort() , pop()
+# reavers = list(input().split())
+# reavers.sort()
+# reavers.pop(0)
+# print(*reavers)
+
+#####  Sublists
+
+# lst = [5.4, 6.7, 10.4]
+# digs = list(map(int,input().split()))
+# lst.append(digs)
+# print(lst)
+
+## append() , list() , []
+# first = list(input().split())
+# second = list(input().split())
+# third = list(input().split())
+# stack = []
+# stack.append(first)
+# stack.append(second)
+# stack.append(third)
+# print(stack)
+
+## append() , list,map()
+# lists = []
+# first = list(map(int,input().split()))
+# second = list(map(int,input().split()))
+# third = list(map(int,input().split()))
+# lists.append(first)
+# lists.append(second)
+# lists.append(third)
+#
+# print(lists[0][3],lists[1][3],lists[2][3])
+
+## in
+# t = [["Скажи-ка", "дядя", "ведь", "не", "даром"],
+#      ["Я", "Python", "выучил", "с", "каналом"],
+#      ["Балакирев", "что", "раздавал?"]]
+#
+# # здесь продолжайте программу
+# word = input()  // дядя
+#
+# print(word in t[0] + t[1] + t[2])
